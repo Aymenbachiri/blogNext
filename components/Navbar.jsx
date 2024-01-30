c68ee5677dfde1dd2a421005716ee026028e17ed
Aymenbachiri <aymen.bachiri99@gmail.com>
2024-01-30 10:27:18 +0100

    first

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,16 +15,7 @@ export default function Navbar() {
             <Link href="/">Home</Link>
           </li>
           <li>
-            <Link href="/portfolio">Portfolio</Link>
-          </li>
-          <li>
             <Link href="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
           </li>
           <li>
             <Link href="/dashboard">Dashboard</Link>
